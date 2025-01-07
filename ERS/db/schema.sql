089076a9d87f20c963a2132fa6688944c0c950bb
mahithap09 <pmahithareddy730@gmail.com>
2025-01-07 13:39:38 +0530

    Update schema.sql

--- ERS/db/schema.sql
+++ ERS/db/schema.sql
@@ -1,1 +1,20 @@
+CREATE DATABASE elective_system;
 
+USE elective_system;
+
+CREATE TABLE hod (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
+
+
+CREATE TABLE elective_selections (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    student_id VARCHAR(5),
+    elective_id INT,
+    student_name VARCHAR(255),
+    department VARCHAR(255),
+    FOREIGN KEY (student_id) REFERENCES students(id),
+    FOREIGN KEY (elective_id) REFERENCES electives(id)
+);
